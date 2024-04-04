58ec40dd3d0f4fd82515b38a02e04906247f6340
Austin Petron <petron64@gmail.com>
2024-04-03 21:32:09 -0500

    fixed menu css

--- front-end/src/layout/Layout.js
+++ front-end/src/layout/Layout.js
@@ -13,9 +13,9 @@ import "./Layout.css";
  */
 function Layout() {
   return (
-    <div className="container-fluid">
+    <div className="container-fluid main-body">
       <div className="row h-100">
-        <div className="col-md-2">
+        <div className="col-md-2 side-bar">
           <Menu />
         </div>
         <div className="col">
